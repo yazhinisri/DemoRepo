bbc527547893d7717187d502e50fe5c91ad2d621
yazhinisri <yazhinisri@gmail.com>
2020-08-29 12:23:58 +0530

    File added

---
+++ src/main/java/com/GitDemo/GitDemo/App.java
@@ -0,0 +1,13 @@
+package com.GitDemo.GitDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
